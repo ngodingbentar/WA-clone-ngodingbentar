37f1d4e16c2d4a350e070d9eb31e8a89a6b053b1
ngodingbentar <dewaqintoro@gmail.com>
2022-03-13 03:53:55 +0700

    sidebar: header & search

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar/>
+        {/* chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYufU19v7GbcdH8zlSENpx_D_yslA-kR0",
+  authDomain: "vuechat-9eb19.firebaseapp.com",
+  databaseURL: "https://vuechat-9eb19-default-rtdb.firebaseio.com",
+  projectId: "vuechat-9eb19",
+  storageBucket: "vuechat-9eb19.appspot.com",
+  messagingSenderId: "955052840025",
+  appId: "1:955052840025:web:3318e575f5a103de72cd48"
+};
